fbea0d05969d41e81b11aa4e5d25ba1e0d755dbd
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2019-04-16 20:37:35 +0200

    Rename controller

--- src/Controller/EnMarche/CommitteeSpaceController.php
+++ src/Controller/EnMarche/CommitteeSpaceController.php
@@ -6,7 +6,7 @@ use AppBundle\Entity\Adherent;
 use AppBundle\Entity\CommitteeMembership;
 use AppBundle\Repository\CommitteeRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -16,7 +16,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  *
  * @Security("is_granted('ROLE_SUPERVISOR') or is_granted('ROLE_HOST')")
  */
-class SupervisorSpaceController extends Controller
+class CommitteeSpaceController extends AbstractController
 {
     /**
      * @param UserInterface|Adherent $adherent
